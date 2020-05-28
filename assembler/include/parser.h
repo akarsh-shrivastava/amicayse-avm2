55b788a10da82a4b40d28df1a90d6c1533d9b1a8
akarsh-shrivastava <akarsh.mails@rediffmail.com>
2020-05-28 21:33:27 +0530

    started parser

--- assembler/include/parser.h
+++ assembler/include/parser.h
@@ -1,1 +1,20 @@
-#include "tokentype.h"
+#ifndef PARSER_H_INCLUDED
+    #define PARSER_H_INCLUDED
+    #include <amicayse/parsegen.h>
+    #include "tokentype.h"
+    extern std::vector<Token> tokens;
+
+    enum amicayse::NonTerminal : int {
+        s=std::numeric_limits<int>::min(),
+        inst, inst_,
+        ops, ops_, op,
+        numconst
+    };
+
+    class Parser{
+        ParseGen parsegen;
+        public:
+        Parser();
+        ParseTreeNode* get_tree();
+    };
+#endif
---
+++ assembler/src/cpp/parser.cpp
@@ -0,0 +1,44 @@
+#include "parser.h"
+
+Parser::Parser(): parsegen(0){
+    std::map<NonTerminal, std::vector<std::vector<int>>> rules = {
+        {s    ,   {{inst, s},  {EPSILON}}
+        },
+        {inst ,   {{IDENTIFIER, inst_, DELIMITOR}, {DELIMITOR}}
+        },
+        {inst_,   {{COLON},  {ops},  {EPSILON}}
+        },
+        {ops  ,   {{op, ops_}, {EPSILON}}
+        },
+        {ops_ ,   {{COMMA, op, ops_}, {EPSILON}}
+        },
+        {op   ,   {{DOLLARSIGN, numconst}, {OPAR, numconst, CPAR}, {OBKT, numconst, CBKT}, {IDENTIFIER}}
+        },
+        {numconst,{{HEXCONST}, {DECCONST}, {CHRCONST}}
+        }
+    };
+
+    parsegen.set_rules(rules);
+    parsegen.set_epsilon(EPSILON);
+    parsegen.set_dollar(DOLLAR);
+    parsegen.set_start_symbol(s);
+    parsegen.calc_first();
+    parsegen.calc_follow();
+
+    parsegen.set_tokens(tokens);
+
+    parsegen.print_rules();  std::cout<<"\n";
+    parsegen.print_first();  std::cout<<"\n";
+    parsegen.print_follow(); std::cout<<"\n";
+    parsegen.print_table();  std::cout<<"\n";
+}
+
+ParseTreeNode* Parser::get_tree(){
+    try{
+        return parsegen.get_tree();
+    }
+    catch (ParserException p){
+        std::cerr<<p.what();
+    }
+    return NULL;
+}
