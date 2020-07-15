5681c6c509341a4efe0ae01a23a51a64efe7c09a
akarsh-shrivastava <akarsh.mails@rediffmail.com>
2020-07-16 02:58:19 +0530

    added read from input file

--- runtime/src/main.c
+++ runtime/src/main.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 #include "memory.h"
 #include "instructions.h"
 #include "execute.h"
@@ -58,10 +61,62 @@ void sample_prog(){
 
     *(seg_reg[REG_CS]+42) = INST_EXIT;    
 }
+void load_program(const char *filename){
+    FILE *fptr;
+    int len = strlen(filename); 
+    if(strcmp(filename+len-5, ".avm2")){
+        fprintf(stderr, "Error: Invalid file extension\n");
+        exit(-1);
+    }
+    fptr = fopen(filename, "r");
+    if(!fptr){
+        fprintf(stderr, "Error: File not found\n");
+        exit(-1);
+    }
+    if(fseek(fptr, 0, SEEK_END)){
+        fprintf(stderr, "Error: Error in reading the file\n");
+        exit(-1);
+    }
+    int size = ftell(fptr);
+    if(size>258){
+        fprintf(stderr, "Too big file\n");
+        exit(-1);
+    }
+    fseek(fptr, 0, SEEK_SET);
+    char magic[5]={0};
+    for (int i=0; i<4; ++i){
+        magic[i] = fgetc(fptr);
+    }
+    if(strcmp(magic, "avm2")){
+        fprintf(stderr, "Error: Unrecognised file format\n");
+        exit(-1);
+    }
+    for(int i=0; true; ++i){
+        char ch = fgetc(fptr);
+        if(ch==EOF)
+            break;
+        *(seg_reg[REG_CS]+i) = ch;
+    }
+}
 void main(int argc, const char **argv){
     init_registers();
     init_inst_set_ptr();
-    sample_prog();
+
+    /*printf("%d\n", argc);
+    for(int i=1; i<argc; ++i){
+        
+    }*/
+    if(argc==1){
+        fprintf(stderr, "Error: No file provided\n");
+        exit(-1);
+    }
+    else if(argc==2);
+    else{
+        fprintf(stderr, "Error: Too many arguments\n");
+        exit(-1);
+    }
+
+    load_program(argv[1]);
     logfile = fopen("logs", "w");
     execute(LOG_IP | LOG_DS | LOG_SS | LOG_CS);
     fclose(logfile);
